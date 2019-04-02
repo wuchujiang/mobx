b055fc332d6f20f2c8224ea2395c2f43fd7adf1e
wcj <344259261@qq.com>
2019-04-02 22:33:13 +0800

    增加插件

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { observable } from 'mobx';
+import { observable, autorun, action, runInAction } from 'mobx';
 import axios from './axios';
 import './mobx.scss';
 
 
 class Store {
   @observable num = 1;
+
+  @observable arr = [1, 2, 3];
+
+  @action.bound setArr() {
+    this.arr.push(4);
+  }
+
+  @action sleep() {
+    this.arr.push()
+  }
 }
+
+autorun((s) => {
+  console.log('run');
+})
+
+// axios.get('/list');
+const store = new Store();
+store.num = 3;
+// const setArr = store.setArr;
+// setArr();
+store.sleep();
 
 class App extends Component {
   componentDidMount = () => {
-    axios.get('/list');
+
   }
 
   render() {
