527e95e81c39334f0ec3a64fca1dcf54f6b3e0d6
wcj <344259261@qq.com>
2019-04-02 21:11:23 +0800

    删除文件

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 //设置允许跨域访问该服务.
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observable } from 'mobx';
+import axios from './axios';
+import './mobx.scss';
+
+
+class Store {
+  @observable num = 1;
+}
 
 class App extends Component {
+  componentDidMount = () => {
+    axios.get('/list');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        test
       </div>
     );
   }
